585a10e2fe68e1619104c8d2489e24e127b9b834
kunal-prakash <kp250796@gmail.com>
2024-08-16 22:06:17 +0530

    React: Emmet, CDN, crossorigin script, react reactDOM, async differ

---
+++ React (Namaste_react)/Ch-01(Inception)/Code/App.js
@@ -0,0 +1,22 @@
+const heading = React.createElement(
+  "div", //Which element is to be created?
+  { id: "child1" }, //Setting attributes to the element created
+  //Siblings in an array
+  [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]
+);
+
+console.log(heading); //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+/*
+render() => renders a JS object(heading) and 
+creates the element which the browser understands and puts it up in the DOM.
+*/
+root.render(heading);
+
+//ReactElement(Object) => HTML(Browser understands)
+
+// For writing clean code in react ********JSX*******
