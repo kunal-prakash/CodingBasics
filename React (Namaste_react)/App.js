25184f78e8f01be1f75190ac412ece52800a3348
kunal-prakash <kp250796@gmail.com>
2024-08-17 05:42:13 +0530

    React: npm, npx, parcel, browserslist

--- React (Namaste_react)/App.js
+++ React (Namaste_react)/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "div", //Which element is to be created?
   { id: "child1" }, //Setting attributes to the element created
