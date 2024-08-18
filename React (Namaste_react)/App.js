8d4a953ceb47a35bd4289f6f96fafb07949542fd
kunal-prakash <kp250796@gmail.com>
2024-08-18 16:37:55 +0530

    React: babel, jsx, reactElement, reactComponent, Component composition

--- React (Namaste_react)/App.js
+++ React (Namaste_react)/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//React.createElement => ReactElement(JS Object) => HTMLElement(render)
+
+//JSX =>Babel transpiles it to React.createElement => ReactElement(JS Object) => HTMLElement(render)
+
 const heading = React.createElement(
   "div", //Which element is to be created?
   { id: "child1" }, //Setting attributes to the element created
@@ -16,10 +20,13 @@ console.log(heading); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 /*
 render() => renders a JS object(heading) and 
-creates the element which the browser understands and puts it up in the DOM.
+creates the element which the browser understands(html) and puts it up in the DOM.
 */
 root.render(heading);
 
-//ReactElement(Object) => HTML(Browser understands)
+// JSX --> It is a convention where we merge JS and html(Not part of React/ Seperate Entity).
+//         is not HTML inside JS.
+//         HTML like, XML like syntax.
+//         Transpiled before reaching JS Engine. (Babel)
 
-// For writing clean code in react ********JSX*******
+const jsxHeading = <h1>Hi I'm JSX</h1>;
